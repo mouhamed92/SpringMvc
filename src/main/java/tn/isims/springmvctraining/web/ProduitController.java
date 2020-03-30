f9175f7b55b78abdc99fae911eddd47c759c3f22
mouhamed92 <hamamimohamed92@gmail.com>
2020-03-30 19:10:04 +0100

    spring mvc

--- src/main/java/tn/isims/springmvctraining/web/ProduitController.java
+++ src/main/java/tn/isims/springmvctraining/web/ProduitController.java
@@ -1,12 +1,14 @@
 package tn.isims.springmvctraining.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import tn.isims.springmvctraining.dao.ProduitRepository;
 import tn.isims.springmvctraining.entities.Produit;
-
 import java.util.List;
 
 @Controller
@@ -15,15 +17,32 @@ public class ProduitController {
     @Autowired
     ProduitRepository produitRepository ;
 
+
     @GetMapping(path = "index")
     public String index(){
         return "index" ;
     }
 
     @GetMapping(path = "produits")
-    public String produits(Model model){
-        List<Produit> produits = produitRepository.findAll() ;
-        model.addAttribute("ListeProduit",produits);
+    public String produits(Model model,
+                           @RequestParam(name ="page",defaultValue = "0") int page ,
+                           @RequestParam(name = "size",defaultValue = "5") int size,
+                           @RequestParam(name = "motC",defaultValue = "")String mc){
+        Page<Produit> produits = produitRepository.findByDesignationContains(mc,PageRequest.of(page,size)) ;
+        model.addAttribute("pageProduit",produits);
+        model.addAttribute("size",size) ;
+        model.addAttribute("motC",mc);
+        model.addAttribute("Currentpage",page) ;
+        model.addAttribute("pages",new int[produits.getTotalPages()]);
         return "produits" ;
+    }
+
+    @GetMapping(path = "delete")
+    public String delete(long id ,
+                         @RequestParam(name ="page",defaultValue = "0") int page ,
+                         @RequestParam(name = "size",defaultValue = "5") int size,
+                         @RequestParam(name = "motC",defaultValue = "")String mc){
+        produitRepository.deleteById(id);
+        return "redirect:produits?page="+page+"&size="+size+"&motC="+mc ;
     }
 }
